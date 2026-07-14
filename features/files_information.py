4f8c8a46d65a175d42da7dcc4736cce5364ae787
Keinzou-cl <bharnelkein051902@gmail.com>
2026-07-14 18:41:23 +0800

    Improvised the filepath_validation function, created and improvised the list_directory function, and created the search_content function)

--- features/files_information.py
+++ features/files_information.py
@@ -5,35 +5,60 @@ HOME_DIR = Path.home()
 
 def filepath_validation(path):
     clean_filepath = Path(path).resolve()
-    boundary_check = [None, ""]
-    fileexistence_check = [None, ""]
-
+    result = {"valid_boundary": None,
+              "message": "",
+              "file_exists": None,
+              "resolved_filepath": clean_filepath}
 
     if clean_filepath.is_relative_to(HOME_DIR):
-        boundary_check[0] = True
-        boundary_check[1] = f'"{clean_filepath}" is within the home directory and is a "safe" filepath'
+        result["valid_boundary"] = True
+        result["message"] = f'"{clean_filepath}" is within the home directory and is a "safe" filepath'
         if clean_filepath.exists():
-            fileexistence_check[0] = True
-            fileexistence_check[1] = f'"{clean_filepath}" is a valid and workable filepath.'
+            result["file_exists"] = True
+            result["message"] = f'"{clean_filepath}" is safe, valid, and a workable filepath.'
         else:
-            fileexistence_check[0] = False
-            fileexistence_check[1] = f'"{clean_filepath}" is an invalid filepath.'
-        return fileexistence_check[0], fileexistence_check[1]
+            result["file_exists"] = False
+            result["message"] = f'"{clean_filepath}" is an invalid filepath.'
     else:
-        boundary_check[0] = False
-        boundary_check[1] = f'The filepath "{clean_filepath}" is outside the safe domain and is deemed a "dangerous" filepath.'
-    return boundary_check[0], boundary_check[1], clean_filepath
+        result["valid_boundary"] = False
+        result["message"] = f'The filepath "{clean_filepath}" is outside the safe domain and is deemed a "dangerous" filepath.'
+
+    return result
     
 def list_directory(path):
-    valid_state, feedback, resolved_path = filepath_validation(path)
-    if valid_state:
-        if resolved_path.is_dir():
-            contents = list(resolved_path.iterdir())
-            return contents
+    result = {"status": None,
+              "message": "",
+              "contents": []
+    }
+    verify_filepath = filepath_validation(path)
+    if verify_filepath["valid_boundary"] and verify_filepath["file_exists"]:
+        if verify_filepath["resolved_filepath"].is_dir():
+            result["status"] = True 
+            result["message"] = "The filepath is a valid directory"
+            result["contents"] = list(verify_filepath["resolved_filepath"].iterdir())
         else:
-            raise Exception("The filepath is not a valid directory")
-    raise Exception(feedback)
+            result["status"] = False
+            result["message"] = "The filepath is not a valid directory"
+    else:
+        result["status"] = False
+        result["message"] = verify_filepath["message"]
+        
+    return result
 
-def search_content(content):
-    pass
+def search_content(name):
+    content = f"*{name}*"
+    all_contents = HOME_DIR.rglob(content)
+    results = {"found": None,
+               "status": "",
+               "matches": []}
+
+    results["matches"] = list(all_contents)
     
+    if len(results["matches"]) == 0:
+        results["found"] = False
+        results["status"] = f'No matches were found with the content name "{name}".'
+    else:
+        results["found"] = True
+        results["status"] = f'There are a total number of {len(results["matches"])} content(s) found with the content name "{name}".'
+    
+    return results
